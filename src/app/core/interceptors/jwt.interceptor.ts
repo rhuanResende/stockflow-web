b837d695d659a8b110593f5b6edd5fff59fb0f0f
rhuan <rhuan.resende@hotmail.com.br>
2026-07-25 02:01:20 -0300

    docs: 1.0.1

--- src/app/core/interceptors/jwt.interceptor.ts
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -1,55 +1,38 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { TokenStorageService } from '../services/token-storage.service';
 import { catchError, switchMap, throwError } from 'rxjs';
 import { AuthService } from '../../shared/services/auth.service';
-import { RefreshTokenRequest } from '../../shared/models/auth.model';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const tokenStorage = inject(TokenStorageService);
   const authService = inject(AuthService);
-  const token = tokenStorage.getToken();
 
-  if (token) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
+  const request = req.clone({
+    withCredentials: true,
+  });
 
-  return next(req).pipe(
+  return next(request).pipe(
     catchError((error) => {
-
       if (error.status !== 401) {
         return throwError(() => error);
       }
 
-      const body: RefreshTokenRequest = {
-        refreshToken: tokenStorage.getRefreshToken()
+      if (request.url.includes('/auth/login')) {
+        return throwError(() => error);
       }
 
-      return authService.refreshToken(body).pipe(
+      if (request.url.includes('/auth/refresh')) {
+        return throwError(() => error);
+      }
 
-        switchMap(res => {
-
-          tokenStorage.saveToken(res.data.accessToken);
-          tokenStorage.saveRefreshToken(res.data.refreshToken);
-          tokenStorage.saveLoginData(res.data);
-
-          const retryReq = req.clone({
-            setHeaders: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-
-          return next(retryReq);
+      return authService.refreshToken().pipe(
+        switchMap(() => {
+          return next(request);
         }),
 
-        catchError(refreshError => {
-          tokenStorage.clear();
+        catchError((refreshError) => {
+          authService.logout();
           return throwError(() => refreshError);
-        })
+        }),
       );
     }),
   );
